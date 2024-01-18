da989889b0dc1b3a5510f2974f3b8bfde59d0685
kyeon06 <jkyeon06@gmail.com>
2024-01-19 04:09:20 +0900

    :sparkles: feat: security config 설정

---
+++ src/main/java/com/yuyun/choiceapp/config/WebSecurityConfig.java
@@ -0,0 +1,71 @@
+package com.yuyun.choiceapp.config;
+
+import com.yuyun.choiceapp.jwt.JwtAccessDeniedHandler;
+import com.yuyun.choiceapp.jwt.JwtAuthenticationEntryPoint;
+import com.yuyun.choiceapp.jwt.JwtSecurityConfig;
+import com.yuyun.choiceapp.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/h2-console/**", "/favicon.ico");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                // token을 사용하는 방식이기 때문에 csrf를 disable
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                )
+                // authorizeHttpRequest : HttpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        // 인증 없이 접근을 허용하겠다는 의미
+                        .requestMatchers("/api/authenticate", "/api/signup", "/api/login").permitAll()
+                        .requestMatchers(PathRequest.toH2Console()).permitAll()
+                        // 나머지 요청에 대해서는 인증을 해야한다라는 의미
+                        .anyRequest().authenticated())
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                // enable h2-console
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+
+                .with(new JwtSecurityConfig(tokenProvider), customizer -> {});
+        return http.build();
+    }
+}
