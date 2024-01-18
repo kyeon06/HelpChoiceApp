69427f8b711c8fae203f21cc310559b6284e237a
kyeon06 <jkyeon06@gmail.com>
2024-01-19 03:13:57 +0900

    :truck: build: project setting

---
+++ src/main/java/com/yuyun/choiceapp/ChoiceappApplication.java
@@ -0,0 +1,13 @@
+package com.yuyun.choiceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoiceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoiceappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuyun/choiceapp/ChoiceappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuyun.choiceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoiceappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
