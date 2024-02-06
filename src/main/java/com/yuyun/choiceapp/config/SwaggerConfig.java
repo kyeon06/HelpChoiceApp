5221964d9f1a1a38730b4865c0b938160c54f40b
kyeon06 <jkyeon06@gmail.com>
2024-02-06 14:22:35 +0900

    :bug: fix: swagger 설정 변경

--- src/main/java/com/yuyun/choiceapp/config/SwaggerConfig.java
+++ src/main/java/com/yuyun/choiceapp/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,6 +26,7 @@ public class SwaggerConfig {
         SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
                 .security(Arrays.asList(securityItem));
     }
